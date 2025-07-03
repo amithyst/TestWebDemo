20f571bedcade1f5987570ba03fb3c6800553008
Kaijie Du <2986723251@qq.com>
2025-07-03 14:49:25 +0800

    更新

--- README.md
+++ README.md
@@ -79,6 +79,7 @@ pip install pip-tools
 
 ```
 django
+djangorestframework
 selenium
 ```
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=p95@@s7fh=y-wfj+(=#$*h)c77msg)8+6m39gl_4v-jt^nhd0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jackdu.pythonanywhere.com', 'www.jackdu.pythonanywhere.com']
 
 
 # Application definition
